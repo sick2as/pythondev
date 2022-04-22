cd99ddea756f1b87c4d0e71bb604fa7a9638193a
sick2as <103950270+sick2as@users.noreply.github.com>
2022-04-21 19:48:26 -0500

    Update p.py

--- p.py
+++ p.py
@@ -1,4 +1,5 @@
 import os
+#NOTE: only use this if you are going to run the file externally
 def findpath(path,file):
     os.system("python (what ever you named the file)")
     # here make sure to remove the perenthis  here ^ and
